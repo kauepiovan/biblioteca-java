00be54be9631b677206b002fc4a8632496922fed
Kaue Piovan <kaupiovan@gmail.com>
2026-01-15 19:38:28 -0300

    Bibliotecario Controller

---
+++ src/main/java/br/com/kauepiovan/biblioteca/controller/BibliotecarioController.java
@@ -0,0 +1,42 @@
+package br.com.kauepiovan.biblioteca.controller;
+
+import java.util.Scanner;
+
+import br.com.kauepiovan.biblioteca.services.BibliotecarioService;
+
+public class BibliotecarioController {
+    private BibliotecarioService bibliotecarioService;
+    private Scanner input;
+
+    public BibliotecarioController(BibliotecarioService bibliotecarioService, Scanner input) {
+        this.bibliotecarioService = bibliotecarioService;
+        this.input = input;
+    }
+
+    public void cadastrar() {
+        try {
+            input.nextLine();
+            System.out.println("Digite o nome do bibliotecario: ");
+            var nomeBibliotecario = input.nextLine();
+            System.out.println("Digite o email do bibliotecario: ");
+            var emailBibliotecario = input.nextLine();
+            bibliotecarioService.cadastrarBibliotecario(nomeBibliotecario, emailBibliotecario);
+            System.out.println(
+                    "Bibliotecario: | " + nomeBibliotecario + " | " + emailBibliotecario + " | Criado com sucesso!");
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public void listar() {
+        try {
+            bibliotecarioService.listarBibliotecarios();
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    // TODO - remover
+
+    // TODO - Atualizar
+}
