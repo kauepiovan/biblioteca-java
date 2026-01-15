18775157d1093f019612a6d6ee7a2797bebc26ed
Kaue Piovan <kaupiovan@gmail.com>
2026-01-15 19:39:17 -0300

    Ajustar para receber os controllers

--- src/main/java/br/com/kauepiovan/biblioteca/application/App.java
+++ src/main/java/br/com/kauepiovan/biblioteca/application/App.java
@@ -1,9 +1,12 @@
 package br.com.kauepiovan.biblioteca.application;
 
 import java.util.Scanner;
-import java.util.UUID;
 
 import br.com.kauepiovan.biblioteca.application.ui.Menu;
+import br.com.kauepiovan.biblioteca.controller.BibliotecarioController;
+import br.com.kauepiovan.biblioteca.controller.EmprestimoController;
+import br.com.kauepiovan.biblioteca.controller.LivroController;
+import br.com.kauepiovan.biblioteca.controller.UsuarioController;
 import br.com.kauepiovan.biblioteca.domain.enums.GeneroLiterario;
 import br.com.kauepiovan.biblioteca.domain.enums.TipoUsuario;
 import br.com.kauepiovan.biblioteca.domain.model.Usuario;
@@ -20,6 +23,8 @@ import br.com.kauepiovan.biblioteca.domain.model.Bibliotecario;
 
 public class App {
     public static void main(String[] args) {
+        var input = new Scanner(System.in);
+        var menu = new Menu();
 
         var usuarioRepository = new UsuarioRepositoryImpl();
         var usuarioService = new UsuarioService(usuarioRepository);
@@ -32,6 +37,11 @@ public class App {
         
         var emprestimoRepository = new EmprestimoRepositoryImpl();
         var emprestimoService = new EmprestimoService(usuarioRepository, livroRepository, emprestimoRepository, bibliotecarioRepository);
+
+        var usuarioController = new UsuarioController(usuarioService, input);
+        var bibliotecarioController = new BibliotecarioController(bibliotecarioService, input);
+        var livroController = new LivroController(livroService, input);
+        var emprestimoController = new EmprestimoController(emprestimoService, input);
 
         // --- Dados mockados iniciais ---
         usuarioRepository.save(new Usuario("Alice Silva", "alice@example.com", TipoUsuario.COMUM));
@@ -46,142 +56,24 @@ public class App {
         bibliotecarioRepository.save(new Bibliotecario("Joao Admin", "joao@biblioteca.com"));
         // -------------------------------
 
-        var menu = new Menu();
-        var input = new Scanner(System.in);
-
-        int userInput = 1;
-        while (userInput != 0) {
+        int opcao;
+        do {
             menu.showStart();
-            userInput = input.nextInt();
+            opcao = input.nextInt();
+            input.nextLine();
 
-            switch (userInput) {
-                case 1:
-                    // Cadastrar usuario
-                    try {
-                        input.nextLine();
-
-                        System.out.println("Digite o nome do usuario: ");
-                        var nomeUsuario = input.next();
-
-                        System.out.println("Digite o email do usuario: ");
-                        var emailUsuario = input.next();
-
-                        System.out.println("especifique o tipo { 1 - Usuario comum | 2 - Usuario premium }");
-                        var tipoUsuario = input.nextInt() == 1 ? TipoUsuario.COMUM : TipoUsuario.PREMIUM;
-
-                        usuarioService.cadastrarUsuario(nomeUsuario, emailUsuario, tipoUsuario);
-                        System.out.println("Usuario: | " + nomeUsuario + " | " + emailUsuario + " | " + tipoUsuario
-                                + " | " + "Criado com sucesso!");
-                    } catch (Exception e) {
-                        System.out.println("Error: " + e.getMessage());
-                    }
-                    break;
-                case 2:
-                    // Cadastrar bibliotecario
-                    try {
-                        input.nextLine();
-                        System.out.println("Digite o nome do bibliotecario: ");
-                        var nomeBiblio = input.nextLine();
-                        System.out.println("Digite o email do bibliotecario: ");
-                        var emailBiblio = input.nextLine();
-                        bibliotecarioService.cadastrarBibliotecario(nomeBiblio, emailBiblio);
-                        System.out.println("Bibliotecario: | " + nomeBiblio + " | " + emailBiblio + " | Criado com sucesso!");
-                    } catch (Exception e) {
-                        System.out.println("Error: " + e.getMessage());
-                    }
-                    break;
-                case 3:
-                    // Cadastrar livros
-                    try {
-                        input.nextLine();
-
-                        System.out.println("Digite o titulo do livro: ");
-                        var tituloLivro = input.nextLine();
-
-                        System.out.println("Digite o autor do livro: ");
-                        var autorLivro = input.nextLine();
-
-                        System.out.println("Selecione o categoria do livro: ");
-                        System.out.println(
-                                "[ 1 - Suspense | 2 - Acao e Aventura | 3 - Fantasia | 4 - ficção científica ]");
-                        var inputCategoria = input.nextInt();
-                        var categoriaLivro = inputCategoria == 1 ? GeneroLiterario.SUSPENSE
-                                : inputCategoria == 2 ? GeneroLiterario.ACAO_E_AVENTURA
-                                        : inputCategoria == 3 ? GeneroLiterario.FANTASIA
-                                                : GeneroLiterario.FICCAO_CIENTIFICA;
-
-                        livroService.cadastrarLivro(tituloLivro, autorLivro, categoriaLivro);
-                    } catch (Exception e) {
-                        System.out.println("Error: " + e.getMessage());
-                    }
-                    break;
-                case 4:
-                    // Listar usuarios
-                    try {
-                        usuarioService.listarUsuarios();
-                    } catch (Exception e) {
-                        System.out.println("Error: " + e.getMessage());
-                    }
-                    break;
-                case 5:
-                    // Listar bibliotecarios
-                    try {
-                        bibliotecarioService.listarBibliotecarios();
-                    } catch (Exception e) {
-                        System.out.println("Error: " + e.getMessage());
-                    }
-                    break;
-                case 6:
-                    // Listar livros
-                    try {
-                        livroService.listarLivros();
-                    } catch (Exception e) {
-                        System.out.println("Error: " + e.getMessage());
-                    }
-                    break;
-                case 7:
-                    // Listar emprestimos
-                    try {
-                        emprestimoService.listarEmprestimos();    
-                    } catch (Exception e) {
-                        System.out.println("Erro: " + e.getMessage());
-                    }
-                    break;
-                case 8:
-                    // Emprestar livros
-                    try {
-                        input.nextLine();
-
-                        System.out.println("Digite o email do usuario: ");
-                        var emailUsuarioEmprestimo = input.nextLine();
-
-                        System.out.println("Digite o titulo do livro: ");
-                        var tituloLivroEmprestimo = input.nextLine();
-
-                        System.out.println("Digite o email do bibliotecario responsavel: ");
-                        var emailBibliotecario = input.nextLine();
-
-                        emprestimoService.realizarEmprestimo(emailUsuarioEmprestimo, tituloLivroEmprestimo, emailBibliotecario);
-                    } catch (Exception e) {
-                        System.out.println("Erro: " + e.getMessage());
-                    }
-                    break;
-                case 9:
-                    // Devolver livros
-                    try {
-                        input.nextLine();
-                        System.out.println("Digite o id do emprestimo: ");
-                        var id = input.nextLine();
-                        emprestimoService.finalizarEmprestimo(UUID.fromString(id));
-                    } catch (Exception e) {
-                        System.out.println("Erro: " + e.getMessage());
-                    }
-                    break;
-                default:
-                    break;
+            switch (opcao) {
+                case 1 -> usuarioController.cadastrar();
+                case 2 -> bibliotecarioController.cadastrar();
+                case 3 -> livroController.cadastrar();
+                case 4 -> usuarioController.listar();
+                case 5 -> bibliotecarioController.listar();
+                case 6 -> livroController.listar(); 
+                case 7 -> emprestimoController.listar();
+                case 8 -> emprestimoController.cadastrar();
+                case 9 -> emprestimoController.concluir();
             }
-        }
+        } while (opcao != 0);
         input.close();
-
     }
 }
