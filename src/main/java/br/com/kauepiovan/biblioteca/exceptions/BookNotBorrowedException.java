f91ec42a95044cb1dfa9309daaa7a636a67337de
Kaue Piovan <kaupiovan@gmail.com>
2026-01-24 00:40:15 -0300

    refatoracao

--- src/main/java/br/com/kauepiovan/biblioteca/exceptions/BookNotBorrowedException.java
+++
@@ -1,7 +0,0 @@
-package br.com.kauepiovan.biblioteca.exceptions;
-
-public class BookNotBorrowedException extends Exception {
-    public BookNotBorrowedException() {
-        super("Exception: Book is not being borrow to this user.");
-    }
-}
