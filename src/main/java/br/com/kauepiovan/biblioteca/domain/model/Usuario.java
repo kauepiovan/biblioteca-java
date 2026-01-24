4270a2963a2ca6584ad4b1d1b37f80b721f36634
Kaue Piovan <kaupiovan@gmail.com>
2026-01-24 00:40:09 -0300

    refatoracao

--- src/main/java/br/com/kauepiovan/biblioteca/domain/model/Usuario.java
+++ src/main/java/br/com/kauepiovan/biblioteca/domain/model/Usuario.java
@@ -2,7 +2,6 @@ package br.com.kauepiovan.biblioteca.domain.model;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import br.com.kauepiovan.biblioteca.domain.enums.TipoUsuario;
 
@@ -16,12 +15,6 @@ public class Usuario extends Pessoa {
         this.tipo = tipo;
         this.limiteLivros = tipo == TipoUsuario.COMUM ? 3 : 5;
     }
-
-    Usuario(String nome, String email) {
-        super(nome, email);
-        this.tipo = TipoUsuario.COMUM;
-    }
-
 
     public void criarEmprestimo(Livro livro) {
         this.livrosEmprestados.add(livro);
@@ -43,30 +36,4 @@ public class Usuario extends Pessoa {
         return tipo;
     }
     
-    public void devolverEmprestimo(Livro livro) {
-        this.livrosEmprestados.remove(livro);
-    }
-    
-    @Override
-    public String toString() {
-        return "{\nid=" + getId() + ",\n" +
-                "nome=" + getNome() + ",\n" +
-                "email=" + getEmail() + ",\n" +
-                "tipo=" + tipo + ",\n" +
-                "limite_de_livros=" + limiteLivros + ",\n" +
-                "livros_emprestados=" + livrosEmprestados + "\n}";
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Usuario usuario = (Usuario) o;
-        return Objects.equals(getId(), usuario.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
 }
