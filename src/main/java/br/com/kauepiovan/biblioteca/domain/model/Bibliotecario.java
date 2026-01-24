46dce96ed8fa51d0f9f4ea1957918e201a0a2f1f
Kaue Piovan <kaupiovan@gmail.com>
2026-01-24 00:39:40 -0300

    refatoracao

--- src/main/java/br/com/kauepiovan/biblioteca/domain/model/Bibliotecario.java
+++ src/main/java/br/com/kauepiovan/biblioteca/domain/model/Bibliotecario.java
@@ -1,6 +1,5 @@
 package br.com.kauepiovan.biblioteca.domain.model;
 
-import java.util.Objects;
 
 public class Bibliotecario extends Pessoa{
 
@@ -8,23 +7,4 @@ public class Bibliotecario extends Pessoa{
         super(nome, email);
     }
 
-    @Override
-    public String toString() {
-        return "{\nid=" + getId() + ",\n" +
-               "nome=" + getNome() + ",\n" +
-               "email=" + getEmail() + "\n}";
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Bibliotecario that = (Bibliotecario) o;
-        return Objects.equals(getId(), that.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
 }
