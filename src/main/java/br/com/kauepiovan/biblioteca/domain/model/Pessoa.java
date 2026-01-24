df0d5a9c9a3251ffcd7661cc1512e3e64586a174
Kaue Piovan <kaupiovan@gmail.com>
2026-01-24 00:40:03 -0300

    refatoracao

--- src/main/java/br/com/kauepiovan/biblioteca/domain/model/Pessoa.java
+++ src/main/java/br/com/kauepiovan/biblioteca/domain/model/Pessoa.java
@@ -1,7 +1,6 @@
 package br.com.kauepiovan.biblioteca.domain.model;
 
 import java.util.UUID;
-import java.util.Objects;
 
 public abstract class Pessoa {
     private UUID id;
@@ -24,19 +23,6 @@ public abstract class Pessoa {
         return nome;
     }
     
-    
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Pessoa pessoa = (Pessoa) o;
-        return Objects.equals(id, pessoa.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 
 }
 
