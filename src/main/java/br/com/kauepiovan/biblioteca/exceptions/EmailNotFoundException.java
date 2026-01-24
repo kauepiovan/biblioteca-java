4ed0f2606199fa0a151adc007051b15f0448c080
Kaue Piovan <kaupiovan@gmail.com>
2026-01-24 00:40:20 -0300

    refatoracao

--- src/main/java/br/com/kauepiovan/biblioteca/exceptions/EmailNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.com.kauepiovan.biblioteca.exceptions;
-
-public class EmailNotFoundException extends Exception {
-    public EmailNotFoundException() {
-        super("Exception: Email can not be found or do not exist.");
-    }
-}
